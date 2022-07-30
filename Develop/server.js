92370af3e4a57e9d620f64a08621ae6c225f57a4
Kdenn1 <103376676+Kdenn1@users.noreply.github.com>
2022-07-30 11:11:31 -0700

    added basic sequelize connection

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize('')
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
